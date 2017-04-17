03bbf31e6cb816f735786c8f630f71491c0ab539
Otho Garcia <othogar@gmail.com>
2017-04-17 15:07:42 -0300

    Criado a o CRUD Generate de Permission e Roles

---
+++ app/Http/Controllers/Permission/PermissionsController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers\Permission;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Permission;
+use Illuminate\Http\Request;
+use Session;
+
+class PermissionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+        $keyword = $request->get('search');
+        $perPage = 25;
+
+        if (!empty($keyword)) {
+            $permissions = Permission::where('nome', 'LIKE', "%$keyword%")
+				->orWhere('descricao', 'LIKE', "%$keyword%")
+
+                ->paginate($perPage);
+        } else {
+            $permissions = Permission::paginate($perPage);
+        }
+
+        return view('admin.permissions.index', compact('permissions'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('admin.permissions.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store(Request $request)
+    {
+
+        $requestData = $request->all();
+
+        Permission::create($requestData);
+
+        Session::flash('flash_message', 'Permission added!');
+
+        return redirect('permission/permissions');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function show($id)
+    {
+        $permission = Permission::findOrFail($id);
+
+        return view('admin.permissions.show', compact('permission'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $permission = Permission::findOrFail($id);
+
+        return view('admin.permissions.edit', compact('permission'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function update($id, Request $request)
+    {
+
+        $requestData = $request->all();
+
+        $permission = Permission::findOrFail($id);
+        $permission->update($requestData);
+
+        Session::flash('flash_message', 'Permission updated!');
+
+        return redirect('permission/permissions');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function destroy($id)
+    {
+        Permission::destroy($id);
+
+        Session::flash('flash_message', 'Permission deleted!');
+
+        return redirect('permission/permissions');
+    }
+}
---
+++ app/Http/Controllers/Role/RolesController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Http\Controllers\Role;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Role;
+use App\Permission;
+use Illuminate\Http\Request;
+use Session;
+
+class RolesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+        $keyword = $request->get('search');
+        $perPage = 25;
+
+        if (!empty($keyword)) {
+            $roles = Role::where('nome', 'LIKE', "%$keyword%")
+				->orWhere('descricao', 'LIKE', "%$keyword%")
+
+                ->paginate($perPage);
+        } else {
+            $roles = Role::paginate($perPage);
+        }
+
+        return view('admin.roles.index', compact('roles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('admin.roles.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store(Request $request)
+    {
+
+        $requestData = $request->all();
+
+        Role::create($requestData);
+
+        Session::flash('flash_message', 'Role added!');
+
+        return redirect('role/roles');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function show($id)
+    {
+        $role = Role::findOrFail($id);
+
+        return view('admin.roles.show', compact('role'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $role = Role::findOrFail($id);
+
+        return view('admin.roles.edit', compact('role'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function update($id, Request $request)
+    {
+
+        $requestData = $request->all();
+
+        $role = Role::findOrFail($id);
+        $role->update($requestData);
+
+        Session::flash('flash_message', 'Role updated!');
+
+        return redirect('role/roles');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function destroy($id)
+    {
+        Role::destroy($id);
+
+        Session::flash('flash_message', 'Role deleted!');
+
+        return redirect('role/roles');
+    }
+}
